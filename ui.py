64bd65e027ff45abe3e2e71429142bc9a3f33a04
trl242 <royaurallockhart@gmail.com>
2021-01-27 14:21:04 -0800

    UX frame done

--- ui.py
+++ ui.py
@@ -3,24 +3,36 @@ from tkinter import *
 
 THEME_COLOR = "#375362"
 
-
 class QuizInterface:
 
     def __init__(self):
         self.window = Tk()
         self.window.title("QuiZzLer")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
-        canvas = Canvas(width=800, height=526)
-        canvas.config(bg=THEME_COLOR, highlightthickness=0)
-        canvas.grid(row=0, column=0, columnspan=2)
 
-        cross_image = PhotoImage(file="images/false.png")
-        unknown_button = Button(image=cross_image, highlightthickness=0)
-        unknown_button.grid(row=1, column=0)
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=300, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            150,
+            text="Question Text.",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         check_image = PhotoImage(file="images/true.png")
-        known_button = Button(image=check_image, highlightthickness=0)
-        known_button.grid(row=1, column=1)
+        self.known_button = Button(image=check_image, highlightthickness=0)
+        self.known_button.grid(row=2, column=0)
+
+
+        cross_image = PhotoImage(file="images/false.png")
+        self.unknown_button = Button(image=cross_image, highlightthickness=0)
+        self.unknown_button.grid(row=2, column=1)
+
+
 
 
 
