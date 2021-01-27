d56b7ab4c747ae040779a0ac2c6a0720c4463c9c
trl242 <royaurallockhart@gmail.com>
2021-01-27 15:07:02 -0800

    everything functional

--- ui.py
+++ ui.py
@@ -27,23 +27,50 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         check_image = PhotoImage(file="images/true.png")
-        self.known_button = Button(image=check_image, highlightthickness=0)
-        self.known_button.grid(row=2, column=0)
+        self.true_button = Button(image=check_image, highlightthickness=0, command=self.true_click)
+        self.true_button.grid(row=2, column=0)
 
 
         cross_image = PhotoImage(file="images/false.png")
-        self.unknown_button = Button(image=cross_image, highlightthickness=0)
-        self.unknown_button.grid(row=2, column=1)
+        self.false_button = Button(image=cross_image, highlightthickness=0, command=self.false_click)
+        self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.canvas.config(bg="white")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.config(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_click(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_click(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+        # print("You got it right!")
+        # else:
+        # print("That's wrong.")
 
 
+# print(f"Your current score is: {self.score}/{self.question_number}")
+# print("\n")
 
 
 
