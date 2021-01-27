e5d8674ea8b8db2f3f22a5615e00f49ab81b0956
trl242 <royaurallockhart@gmail.com>
2021-01-27 14:35:37 -0800

    text in UX no true and false function yet

--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
-
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("QuiZzLer")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -17,6 +19,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             150,
+            width=280,
             text="Question Text.",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -32,12 +35,18 @@ class QuizInterface:
         self.unknown_button = Button(image=cross_image, highlightthickness=0)
         self.unknown_button.grid(row=2, column=1)
 
-
-
-
-
-
-
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+
+
+
+
+
+
+
